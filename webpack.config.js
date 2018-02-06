288c2cfd3921a027dd150de2b9eea97bfc9df722
Pixel <louis.vincent.bompart@gmail.com>
2018-02-06 16:36:50 -0500

    Remove hack

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,6 @@ module.exports = {
     },
     plugins:[
         new webpack.ProvidePlugin({
-            'Promise': 'exports?global.Promise!es6-promise/auto',
             'fetch': 'exports?self.fetch!whatwg-fetch'
         }),
         new webpack.optimize.UglifyJsPlugin()
